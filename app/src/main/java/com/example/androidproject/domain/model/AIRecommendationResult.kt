e8b24e8bd1533f92500351c46a0fcf7fe237fa8c
성준민 <hgf8489@gmail.com>
2025-11-06 21:47:49 +0900

    trash

--- app/src/main/java/com/example/androidproject/domain/model/AIRecommendationResult.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidproject.domain.model
-
-data class AIRecommendationResult(
-    val recommendedExercises: List<Exercise>,
-    val recommendedDiets: List<Diet>
-)
--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// build.gradle.kts (Project:YourAppName)
-
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-
-    alias(libs.plugins.hilt.android.gradle.plugin) apply false
-}
